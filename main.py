5d10adc815bbbde6da44a3621fbce56b2cfe7fbf
HanzlaTech <hafeezhanzla24@gmail.com>
2026-09-19 17:16:43 +0500

    add two d in login msg

--- main.py
+++ main.py
@@ -420,7 +420,7 @@ def login(data:LoginSchema,response:Response,request:Request):
     user=cursor.fetchone()
     cursor.close()
     if user is not None:
-     log.info("User exist in database")
+     log.info("User exist in databasedd")
      if password_hash.verify(data.password,user["Password"]):
         log.info("password is hash")
         payload={
