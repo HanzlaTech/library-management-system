71823afd0b7a31fae6fecbf5f35ff6b4c564237d
HanzlaTech <hafeezhanzla24@gmail.com>
2026-09-19 18:11:55 +0500

    Revert "Merge branch 'signup' into login"
    This reverts commit 87e6ad15d3421ce68ba621df3e86a127aaca1d43, reversing
    changes made to 5d10adc815bbbde6da44a3621fbce56b2cfe7fbf.

--- main.py
+++ main.py
@@ -427,7 +427,6 @@ def login(data:LoginSchema,response:Response,request:Request):
             "userid":user["User_id"],
             "role":user["Role"],
             "email":user["Email"],
-            
             "URL":user["URL"],
             "exp":datetime.utcnow()+timedelta(minutes=1)
         }
